9b0444ab26823972858a5ae9a39595be5cc7a6bf
David Cruz <david@typokign.com>
2018-05-03 11:12:31 -0700

    Fix typo in memory consumption

--- masm4.asm
+++ masm4.asm
@@ -222,7 +222,7 @@ extern String_indexOf_1: Near32, String_indexOf_2:Near32, String_indexOf_3:Near3
 .data
 	;;;;;;;;;;;;;;;;;;;; MENU ;;;;;;;;;;;;;;;;;;;;;
 	strMenuTop	byte	4 dup (32), 201,15 dup(205), 181, " MASM 4 TEXT EDITOR ", 198, 14 dup (205),187,13,10,0
-	strMenuMem	byte	4 dup (32), 186, " Data Structure Memory Composition: ", 0
+	strMenuMem	byte	4 dup (32), 186, " Data Structure Memory Consumption: ", 0
 	strMenuByte byte	" bytes ",186,13,10,0
 	strMenuTest byte	"00000000",0 ; todo remove
 	strMenuSep	byte	4 dup (32), 199, 51 dup (196), 182, 13, 10, 0
