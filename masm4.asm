071e365177f4dc3dcae09d1003558edf75d1b3a9
David Cruz <david@typokign.com>
2018-05-02 18:54:21 -0700

    Improved menu, moved mem usage to top as jbarn requested

--- masm4.asm
+++ masm4.asm
@@ -36,7 +36,11 @@ extern String_indexOf_1: Near32, String_indexOf_2:Near32, String_indexOf_3:Near3
 	Line ends
 
 	mMenu macro
-		invoke putstring, addr strMenu0
+		invoke putstring, addr strMenuTop
+		invoke putstring, addr strMenuMem
+		invoke putstring, addr strMenuTest ; todo remove
+		invoke putstring, addr strMenuByte
+		invoke putstring, addr strMenuSep
 		invoke putstring, addr strMenu1
 		invoke putstring, addr strMenu2
 		invoke putstring, addr strMenu2a
@@ -46,24 +50,28 @@ extern String_indexOf_1: Near32, String_indexOf_2:Near32, String_indexOf_3:Near3
 		invoke putstring, addr strMenu5
 		invoke putstring, addr strMenu6
 		invoke putstring, addr strMenu7
-		invoke putstring, addr strMenu8
-		invoke putstring, addr strMenu9
+		invoke putstring, addr strMenuEnd
+	endm
 	endm
 
 .data
 	;;;;;;;;;;;;;;;;;;;; MENU ;;;;;;;;;;;;;;;;;;;;;
-	strMenu0	byte	4 dup (32), 201,7 dup(205)," MASM 4 TEXT EDITOR ", 7 dup (205),187,13,10,0
-	strMenu1	byte	4 dup (32), 186, "<1> View all strings              ",186,13,10,0
-	strMenu2	byte	4 dup (32), 186, "<2> Add String                    ",186,13,10,0
-	strMenu2a	byte	4 dup (32), 186, "    <a> From Keyboard             ",186,13,10,0
-	strMenu2b	byte	4 dup (32), 186, "    <b> From File [input.txt]     ",186,13,10,0
-	strMenu3	byte	4 dup (32), 186, "<3> Delete String                 ",186,13,10,0
-	strMenu4	byte	4 dup (32), 186, "<4> Edit String                   ",186,13,10,0
-	strMenu5	byte	4 dup (32), 186, "<5> String Search                 ",186,13,10,0
-	strMenu6	byte	4 dup (32), 186, "<6> Memory Consumption            ",186,13,10,0
-	strMenu7	byte	4 dup (32), 186, "<7> Save File                     ",186,13,10,0
-	strMenu8	byte	4 dup (32), 186, "<8> Quit                          ",186,13,10,0
-	strMenu9	byte	4 dup (32), 200, 34 dup(205),188,13,10,0
+	strMenuTop	byte	4 dup (32), 201,15 dup(205), 181, " MASM 4 TEXT EDITOR ", 198, 14 dup (205),187,13,10,0
+	strMenuMem	byte	4 dup (32), 186, " Data Structure Memory Composition: ", 0
+	strMenuByte byte	" bytes ",186,13,10,0
+	strMenuTest byte	"00000000",0 ; todo remove
+	strMenuSep	byte	4 dup (32), 199, 51 dup (196), 182, 13, 10, 0
+	strMenu1	byte	4 dup (32), 186, " <1> View all strings", 30 dup (32), 186,13,10,0
+	strMenu2	byte	4 dup (32), 186, " <2> Add String", 36 dup (32), 186,13,10,0
+	strMenu2a	byte	4 dup (32), 186, "     <a> From Keyboard", 29 dup (32),186,13,10,0
+	strMenu2b	byte	4 dup (32), 186, "     <b> From File [input.txt]", 21 dup (32),186,13,10,0
+	strMenu3	byte	4 dup (32), 186, " <3> Delete String", 33 dup (32), 186,13,10,0
+	strMenu4	byte	4 dup (32), 186, " <4> Edit String", 35 dup (32),186,13,10,0
+	strMenu5	byte	4 dup (32), 186, " <5> String Search", 33 dup (32),186,13,10,0
+	strMenu6	byte	4 dup (32), 186, " <6> Save File", 37 dup (32),186,13,10,0
+	strMenu7	byte	4 dup (32), 186, " <7> Quit", 42 dup (32),186,13,10,0
+	strMenuEnd	byte	4 dup (32), 200, 51 dup(205),188,13,10,0
+
 
 	head		dword	0
 	tail		dword	0
@@ -131,22 +139,6 @@ printDocument endp
 
 _main:
 	mMenu
-	push offset strMenu0
-	call addLine
-	add esp, 4
-	push offset strMenu1
-	call addLine
-	add esp, 4
-	push offset strMenu2
-	call addLine
-	add esp, 4
-	push offset strMenu3
-	call addLine
-	add esp, 4
-	push offset strMenu4
-	call addLine
-	add esp, 4
-	call printDocument
 
 	invoke ExitProcess, 0
 end _main
